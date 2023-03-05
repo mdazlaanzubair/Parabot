17d543b12c6bf7990c61ceb1b0fba44a198e36a4
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-03-05 12:46:55 +0500

    final commit

--- src/pages/components/summarizer/SummarizerForm.jsx
+++ src/pages/components/summarizer/SummarizerForm.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useLoader } from "../../../context/LoaderContext";
 import { useSummary } from "../../../context/summary/SummaryContext";
 
